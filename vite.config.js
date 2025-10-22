aa5ee4bd88807f0173ce438a88fa943090253695
neotube-rilts <neotube.rilts@gmail.com>
2025-10-22 21:16:28 +0900

    페이지나오도록 수정중

--- vite.config.js
+++ vite.config.js
@@ -2,9 +2,12 @@
 import { sveltekit } from '@sveltejs/kit/vite';
 import { defineConfig } from 'vite';
 
+const base = process.env.NODE_ENV === 'production' ? '/neotube/' : '/';
+
 export default defineConfig({
     plugins: [sveltekit()],
     // ✨ 다음 server 객체를 추가합니다.
+    base,
     server: {
         host: true, // 0.0.0.0 (모든 네트워크 인터페이스)에 바인딩하도록 설정
         port: 5173, // 포트 명시 (선택 사항)
